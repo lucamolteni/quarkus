27c93f48e5259e7cdb1f6bc4338f34e29d615c1d
Luca Molteni <volothamp@gmail.com>
2025-10-22 11:14:33 +0200

    Test Mixing annotations properly fails (not yet implemented)

--- extensions/hibernate-reactive-transactions/deployment/src/test/java/io/quarkus/hibernate/reactive/transactions/test/mixing/MixWithOnSessionOnDemandTest.java
+++ extensions/hibernate-reactive-transactions/deployment/src/test/java/io/quarkus/hibernate/reactive/transactions/test/mixing/MixWithOnSessionOnDemandTest.java
@@ -1,5 +1,7 @@
 package io.quarkus.hibernate.reactive.transactions.test.mixing;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import jakarta.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
@@ -7,10 +9,12 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.hibernate.reactive.panache.common.WithSessionOnDemand;
 import io.quarkus.hibernate.reactive.panache.common.runtime.AbstractUniInterceptor;
+import io.quarkus.hibernate.reactive.panache.common.runtime.SessionOperations;
 import io.quarkus.hibernate.reactive.panache.common.runtime.WithSessionOnDemandInterceptor;
 import io.quarkus.hibernate.reactive.transactions.deployment.TransactionalInterceptor;
 import io.quarkus.test.QuarkusUnitTest;
 import io.quarkus.test.vertx.RunOnVertxContext;
+import io.quarkus.test.vertx.UniAsserter;
 import io.smallrye.mutiny.Uni;
 
 public class MixWithOnSessionOnDemandTest {
@@ -18,24 +22,29 @@ public class MixWithOnSessionOnDemandTest {
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .withApplicationRoot((jar) -> jar
-                    .addClasses(TransactionalInterceptor.class, AbstractUniInterceptor.class,
-                            WithSessionOnDemandInterceptor.class))
+                    .addClasses(
+                            TransactionalInterceptor.class,
+                            Transactional.class,
+                            AbstractUniInterceptor.class,
+                            WithSessionOnDemandInterceptor.class,
+                            WithSessionOnDemand.class,
+                            SessionOperations.class
+                    ))
             .withConfigurationResource("application.properties");
 
     @Test
     @RunOnVertxContext
-    public void avoidMixingTransactionalAnnotationsTest() {
-        // TODO Luca test is running (so this is not failing) but
-        //  - TransactionalInterceptor is running fine (should fail)
-        //  - WithSessionOnDemandInterceptor is not being called (it should fail anyway)
-        avoidMixingTransactionalAnnotations();
+    public void avoidMixingTransactionalAnnotationsTest(UniAsserter asserter) {
+        Uni<?> uni = avoidMixingTransactionalAnnotations();
+
+        asserter.assertThat(() -> uni, h -> {
+            assertThat(h).isNotNull();
+        });
     }
 
     @Transactional
     @WithSessionOnDemand
-    public Uni avoidMixingTransactionalAnnotations() {
-        System.out.println("++++ here?");
-        throw new UnsupportedOperationException();
+    public Uni<?> avoidMixingTransactionalAnnotations() {
+        throw new UnsupportedOperationException("this shouldn't be called");
     }
-
 }
