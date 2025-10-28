2611433c1cbfa1916c9f592f9d5b63e7b6949912
Luca Molteni <volothamp@gmail.com>
2025-10-28 15:53:53 +0100

    Proper testing of Mix reactive and blocking
    (tests not running)

--- extensions/hibernate-reactive-transactions/deployment/src/test/java/io/quarkus/hibernate/reactive/transactions/test/mixing/MixReactiveBlockingTest.java
+++ extensions/hibernate-reactive-transactions/deployment/src/test/java/io/quarkus/hibernate/reactive/transactions/test/mixing/MixReactiveBlockingTest.java
@@ -1,15 +1,13 @@
 package io.quarkus.hibernate.reactive.transactions.test.mixing;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
 
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.transaction.Transactional;
 
 import org.hibernate.reactive.mutiny.Mutiny;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -17,11 +15,12 @@ import io.quarkus.builder.Version;
 import io.quarkus.hibernate.reactive.transactions.runtime.TransactionalInterceptor;
 import io.quarkus.hibernate.reactive.transactions.test.Hero;
 import io.quarkus.maven.dependency.Dependency;
-import io.quarkus.runtime.BlockingOperationNotAllowedException;
 import io.quarkus.test.QuarkusUnitTest;
 import io.quarkus.test.vertx.RunOnVertxContext;
 import io.quarkus.test.vertx.UniAsserter;
+import io.quarkus.vertx.VertxContextSupport;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 
 public class MixReactiveBlockingTest {
 
@@ -33,7 +32,7 @@ public class MixReactiveBlockingTest {
             ));
 
     @Inject
-    EntityManagerFactory entityManagerFactory;
+    EntityManager entityManager;
 
     @Inject
     Mutiny.SessionFactory reactiveSessionFactory;
@@ -41,49 +40,55 @@ public class MixReactiveBlockingTest {
     @Test
     @RunOnVertxContext
     public void avoidMixingTransactionalAndWithTransactionTest(UniAsserter asserter) {
-        Uni<Void> uni = avoidMixingBlockingEMWithReactiveSessionFactory();
-
-        // TODO Luca this should fail but actually it just disable the blocking transaction and
-        // Uses only the reactive one
-        asserter.assertThat(() -> uni, e -> assertThat(e).isNull());
+        avoidMixingBlockingEMWithReactiveSessionFactory();
+        // assert with Vertx
     }
 
     @Transactional
-    public Uni<Void> avoidMixingBlockingEMWithReactiveSessionFactory() {
+    public Uni<Object> avoidMixingBlockingEMWithReactiveSessionFactory() {
         Hero heroBlocking = new Hero("heroName");
-        entityManagerFactory.createEntityManager().persist(heroBlocking);
+        Uni<Object> uni = Uni.createFrom()
+                .item(() -> {
+                    // We're nesting @Transactional calls but we cannot reuse the same transaction
+                    // As they're of two different kinds
+                    // We're not sure this is testable, it might be in the blocking code we don't have the
+                    // State of the context we need to check this
+                    return blockingOp(heroBlocking);
+                })
+                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
 
-        Hero heroReactive = new Hero("heroName");
-        return reactiveSessionFactory.withSession(s -> s.persist(heroReactive));
-    }
-
-    @Test
-    @RunOnVertxContext
-    public void onlyReactiveWorks(UniAsserter asserter) {
-        Uni<Hero> uni = onlyReactiveWorks();
-
-        asserter.assertThat(() -> uni, h -> assertThat(h).isNotNull());
-    }
-
-    @Transactional
-    public Uni<Hero> onlyReactiveWorks() {
-        Hero heroReactive = new Hero("heroName");
-        return reactiveSessionFactory.withSession(s -> s.merge(heroReactive))
-                .flatMap(h -> reactiveSessionFactory.withSession(s -> s.find(Hero.class, h.id)));
-    }
-
-    @Test
-    public void onlyBlockingWorks() {
-        Hero h = onlyBlockingWorksOperation();
-
-        assertThat(h).isNotNull();
+        return uni;
     }
 
     @Transactional
-    public Hero onlyBlockingWorksOperation() {
+    private @Nullable Object blockingOp(Hero heroBlocking) {
+        entityManager.persist(heroBlocking);
+        return null;
+    }
+
+    @Test
+    public void avoidMixingTransactionalAndWithTransactionTest() throws Throwable {
+        Void v = fromBlockingToReactive();
+
+        // Assert blocking
+    }
+
+    // https://quarkus.io/guides/vertx#executing-asynchronous-code-from-a-blocking-thread
+    @Transactional
+    public Void fromBlockingToReactive() throws Throwable {
+        Hero heroBlocking = new Hero("heroName");
+        entityManager.persist(heroBlocking);
+
         Hero heroReactive = new Hero("heroName");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.persist(heroReactive);
-        return entityManager.find(Hero.class, heroReactive.id);
+
+        return VertxContextSupport.subscribeAndAwait(() -> {
+            return persistReactive(heroReactive);
+        });
+
+    }
+
+    @Transactional
+    private Uni<Void> persistReactive(Hero heroReactive) {
+        return reactiveSessionFactory.withSession(session -> session.persist(heroReactive));
     }
 }
